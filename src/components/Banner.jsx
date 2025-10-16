c180bdfcd16f7aaeee7846428dd76dee02054855
sfronaue <Sophie.Fronauer@edvschule-plattling.de>
2025-10-16 11:55:15 +0200

    Banner changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ function Banner() {
     <>
         <div>
           <div className="relative w-full">
-            <img src="/banner.gif" alt="Banner" className="w-full h-[27%] object-cover"></img>
+            <img src="banner.gif" alt="" className="w-full h-[27%] object-cover"></img>
             </div>
             <div className="absolute left-0 right-0 lg:bottom-130 sm:px-8">
               <h2 className="text-3xl font-bold">Hi! I'm Sophie</h2>
