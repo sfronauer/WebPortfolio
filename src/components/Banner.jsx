26b1318524da29239f102f7c8f35b9d30a8ebc5d
sfronaue <Sophie.Fronauer@edvschule-plattling.de>
2025-10-16 12:36:35 +0200

    small changes in Banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,21 @@
 function Banner() {
-
   return (
     <>
-        <div>
-          <div className="relative w-full">
-            <img src="banner.gif" alt="" className="w-full h-[27%] object-cover"></img>
-            </div>
-            <div className="absolute left-0 right-0 lg:bottom-130 sm:px-8">
-              <h2 className="text-3xl font-bold">Hi! I'm Sophie</h2>
-            </div>
+      <div>
+        <div className="relative w-full">
+          {/* <img src="banner.gif" alt="" className="w-full h-[27%] object-cover"></img> */}
+          <img
+            src={`${import.meta.env.BASE_URL}banner.gif`}
+            alt="Banner"
+            className="w-full h-[27%] object-cover"
+          />
         </div>
+        <div className="absolute left-0 right-0 lg:bottom-130 sm:px-8">
+          <h2 className="text-3xl font-bold">Hi! I'm Sophie</h2>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
